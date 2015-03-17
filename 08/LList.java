d2387fd911fe8f6868056d4aaaee487806449788
david19kerrigan <david19kerrigan@gmail.com>
2015-03-17 08:38:40 -0400

    a

---
+++ 08/LList.java
@@ -0,0 +1,53 @@
+public class LList {
+		private Node l=null;
+		public LList(){
+			l = new Node();
+			private int length;		
+		}
+		public void add(index x, String s){
+			private Node aNode = l;
+			private Node newNode = new Node();
+			for(int i = 0; i < x; i++){
+				aNode = aNode.getNext();
+				if(i == x-1){
+					newNode.setNext(aNode.getNext());
+				}
+			aNode.setNext(newNode);
+				
+			}
+		public void add(String s){
+				Node tmp = new Node(s);
+				tmp.setNext(l);
+				length++;
+				l = tmp;
+		}
+		public String find(String s){
+			private Node aNode = l;
+			while(aNode != null){
+				aNode = aNode.getNext();
+			}
+			if(aNode.getData() == s){
+				return aNode.getData();
+			}
+		}
+		public void remove(index n){
+			private Node aNode = l;
+			for(int i = 0; i < x; i++){
+				aNode = aNode.getNext();
+				if(i == x-1){
+					newNode.setNext(aNode.getNext());
+				}
+			aNode.setNext(null);
+		}
+
+		public String toString(){
+				String s = "";
+				Node tmp;
+				for (tmp = l; tmp != null; tmp=tmp.getNext()){
+						s = s + tmp + " --> ";
+				}
+				s = s + "null";
+				return s;
+		}
+		
+}
