82976f8b87096d8ab490dcef3fc2e9a08af8821f
david19kerrigan <david19kerrigan@gmail.com>
2015-04-23 08:41:21 -0400

    a

--- 14-trees/Tree.java
+++ 14-trees/Tree.java
@@ -3,7 +3,12 @@ public class Tree{
 	public Tree(Node r){
 		root = r;
 	}
-
+	public void insert(Node n){
+		Node tmp = root;
+		myData = n.getData();
+		while(tmp!=null){
+			if(myData > tmp.getData()) tmp = tmp.getNext;
+			
 }
 	
 	
